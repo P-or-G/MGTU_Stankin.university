0e127e652bc1c3bce51147b374b17c30d83698ee
Fedor Gornostaev <102575921+P-or-G@users.noreply.github.com>
2022-12-09 20:23:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
-
+import kbs
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -53,26 +53,12 @@ async def cmd_start(message: types.Message):
     global ID, Name
     ID = message.from_user.id
     Name = message.from_user.first_name
-    #--------------
-    btn1 = InlineKeyboardButton('Архитектура и строительство', callback_data='arch')
-    btn2 = InlineKeyboardButton('Военное дело, безопасность, охрана', callback_data='mil')
-    btn3 = InlineKeyboardButton('Дипломатия', callback_data='dip')
-    btn4 = InlineKeyboardButton('Лингвистика', callback_data='ling')
-    btn5 = InlineKeyboardButton('IT', callback_data='it')
-    btn6 = InlineKeyboardButton('Искусство', callback_data='pnt')
-    btn7 = InlineKeyboardButton('Спорт', callback_data='pe')
-    btn8 = InlineKeyboardButton('Медицина', callback_data='med')
-    btn9 = InlineKeyboardButton('Инженерные технологии', callback_data='eng')
-    btn10 = InlineKeyboardButton('Химико-биологические науки', callback_data='chem')
-    st_but = InlineKeyboardMarkup(row_width=3)
-    #--------------
-    st_but.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10)
-    await bot.send_message(ID, 'Какую специальность ВУЗа выбираете?', reply_markup=st_but)
+
+    await bot.send_message(ID, 'Какую специальность ВУЗа выбираете?', reply_markup=kbs.st_but)
 
 @dp.callback_query_handler(text='arch')
 async def arch_vuz(callback_query: types.CallbackQuery):
-
-    await bot.send_message(callback_query.from_user.id, 'Возможно, вам стоит присмотреться к этим вузам.\nВыберите, какой из них вас интересует', reply_markup=)
+    await bot.send_message(callback_query.from_user.id, 'Возможно, вам стоит присмотреться к этим вузам.\nВыберите, какой из них вас интересует')
 
 if __name__ == "__main__":
     # Запуск бота
