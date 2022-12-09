7f13e75a0954312b93152dc9a6f344652da79bbd
Fedor Gornostaev <102575921+P-or-G@users.noreply.github.com>
2022-12-09 20:25:06 +0300

    Add files via upload

---
+++ kbs.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+    InlineKeyboardButton
+import unicodedata
+
+nokb = ReplyKeyboardRemove()
+
+btn1 = InlineKeyboardButton('Архитектура и строительство', callback_data='arch')
+btn2 = InlineKeyboardButton('Военное дело, безопасность, охрана', callback_data='mil')
+btn3 = InlineKeyboardButton('Дипломатия', callback_data='dip')
+btn4 = InlineKeyboardButton('Лингвистика', callback_data='ling')
+btn5 = InlineKeyboardButton('IT', callback_data='it')
+btn6 = InlineKeyboardButton('Искусство', callback_data='pnt')
+btn7 = InlineKeyboardButton('Спорт', callback_data='pe')
+btn8 = InlineKeyboardButton('Медицина', callback_data='med')
+btn9 = InlineKeyboardButton('Инженерные технологии', callback_data='eng')
+btn10 = InlineKeyboardButton('Химико-биологические науки', callback_data='chem')
+st_but = InlineKeyboardMarkup(row_width=3)
